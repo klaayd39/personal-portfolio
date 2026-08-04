8e05891875d6ac698b18447f0d32d7752ed6d927
Klyde Joseph Yabo <klydejosephy@gmail.com>
2026-08-04 19:25:45 +0800

    Update Resume: Add portfolio website link

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -35,6 +35,8 @@ export default function Resume() {
             <span className="sep">|</span>
             <a href="https://github.com/klaayd39" target="_blank" rel="noreferrer">github.com/klaayd39</a>
             <span className="sep">|</span>
+            <a href="https://yabopersonalportfolio.vercel.app/" target="_blank" rel="noreferrer">yabopersonalportfolio.vercel.app</a>
+            <span className="sep">|</span>
             <span>Malaybalay City, Philippines</span>
           </div>
         </header>
